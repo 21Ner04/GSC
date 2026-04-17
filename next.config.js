46359a89ffb9c31b8d1e5f18eb8d7fe1471e7a08
Developer <dev@example.com>
2026-04-17 20:07:53 +0300

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-          {
+      {
         protocol: 'https',
         hostname: 'gsc-sandy.vercel.app',
         port: '',
